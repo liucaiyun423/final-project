a87532ff637b6c206a988f54ad8206cb9a7164f2
caiyun <liucaiyun423@gmail.com>
2011-11-22 13:47:20 -0500

    Global_variable ui added

--- ui/js/ui/eventBinding.js
+++ ui/js/ui/eventBinding.js
@@ -1,27 +1,49 @@
 $(document).ready(function() {
-
+        var zindex = 1;
 	    $('a.link').click(function () {  
 		$('article').scrollTo($(this).attr('href'), 400);
 		$('#box2').append('<embed width="100%" height="100%" src="Znode/index.html"/>');
 	    $('a.link').removeClass('selected');  
 		$(this).addClass('selected');
 		return false;
-	    });  
+	    });
 		
-		$('#open').click(function () { 
+		  
+		
+		$('#openFiles').click(function () { 
 		  fetchFiles();
-		$('#openWin').fadeIn();
+		$('#openGWin').fadeOut();
+		$('#openFilesWin').fadeIn();
 		});
-		$('#fileContent, #upload').click(function () { 
-		$('#openWin').fadeOut();
+		
+		$('#openGVariables').click(function () { 
+		//  fetchGVariables();
+		$('#openFilesWin').fadeOut();
+		$('#openGWin').fadeIn();
 		});
+		
+		$('#filePane, #upload').mousedown(function () { 
+		$('#openFilesWin').fadeOut();
+		$('#openGWin').fadeOut();
+		});
+		
 		$('#upload').click(function () { 
 		$('#displaybox').show();
 		}); 
+		
 		$('#closeOverlay').click(function () { 
 		$('#displaybox').hide();
-		}); 
-		$('#fileContent .inner img').live("click", function(event){ 
+		});
+		
+		
+		$('#nameForm').submit(function(event) {
+			event.preventDefault();
+			highLight();
+		});//prevent default submit event to refresh the whole page;
+		
+		
+		 
+		$('#filePane .inner img').live("click", function(event){ 
 		$(this).parent().remove();
 		});  
 		
@@ -29,16 +51,19 @@ $(document).ready(function() {
 		event.preventDefault();
 		var fileName=$(this).children().attr('href');
 		$.get('php/getContent.php?fname='+fileName, function(data) {
-		$('#fileContent').append('<div class=inner/>');
+		$('#filePane').append('<div class="inner"/>');
 	    var n = $(".inner").last();
 	    n.append('<img src="images/db.png"/>');
 	    n.append('<p style="background-color:#db0;color:#fff;text-align:center;border-bottom: 1px solid #a80;">'+fileName+"</p>");
-		n.append('<div style="padding:0.8em;" >'+data+'</div>');
+		n.append('<div class="data" style="padding:0.8em;" >'+data+'</div>');
 		$(".inner").first().css({"left":"240"});
-		n.resizable();
-		n.draggable();
+		n.resizable().draggable();
+		n.mouseenter(function(){n.css("z-index", zindex++);});
+	//    $('.data').highlight('function');
 		});
 		}); 
+		
+		
 		
 		$('#submit').live("click", function(event){
 	          event.preventDefault();
@@ -49,6 +74,11 @@ $(document).ready(function() {
   	        var fileList =  $("#files");
               fileList.load("php/getFiles.php");
 			 }
+			 
+		function fetchGVariables(){
+			var gList =  $("#gVariables");
+              gList.load("php/getGVariables.php");
+		}	 
    
         function ajaxFileUpload()
 	{ 
@@ -76,6 +106,14 @@ $(document).ready(function() {
 		)
 		return false;
 
+	}
+	
+	function highLight()
+	{
+		$('.data').removeHighlight();
+		if(($('.data').length>0)&&($('#searchElement').val()!=''))
+		$('.data').highlight($('#searchElement').val());
+		else $('.data').removeHighlight();
 	}
 	
 	});
